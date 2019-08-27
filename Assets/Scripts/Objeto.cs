7fc27cf0d4fb59eb1b8b872701c23f925ee1427a
RaimonKoudsi <ramonkoudsi@gmail.com>
2019-08-26 22:27:13 -0400

    viento

---
+++ Assets/Scripts/Objeto.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objeto : MonoBehaviour {
+
+	public float fuerza;
+	public Vector3 direccion;
+
+	public bool EnZonaViento=false;
+	Rigidbody2D rb;
+
+
+	private void Start()
+	{
+		rb=GetComponent<Rigidbody2D>();
+	}
+
+
+	void FixedUpdate()
+	{
+		if(EnZonaViento)
+		{
+			rb.AddForce(direccion*fuerza);
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D colision)
+	{
+		if(colision.gameObject.tag=="Viento")
+		{
+			EnZonaViento=true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D colision)
+	{
+		if(colision.gameObject.tag=="Viento")
+		{
+			EnZonaViento=false;
+		}
+	}
+
+}
