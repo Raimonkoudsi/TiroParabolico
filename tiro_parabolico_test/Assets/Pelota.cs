d77786151ddad3bfba730985eae36c9a3abec0d7
Mirla M <mirmon.exe@outlook.com>
2019-09-02 22:15:05 -0400

    Nueva escena con cañon y pelotita
    tiene viento, y arco de lanzamiento con puntitos :D

---
+++ tiro_parabolico_test/Assets/Pelota.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota : MonoBehaviour {
+
+	public float fuerza;
+	public Vector3 direccion;
+
+	public bool EnZonaViento=false;
+	Rigidbody rb;
+
+
+	private void Start()
+	{
+		rb=GetComponent<Rigidbody>();
+
+	}
+
+
+	void FixedUpdate()
+	{
+		if(EnZonaViento)
+		{
+			rb.AddForce(direccion*fuerza);
+		}
+	}
+
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+			this.rb.isKinematic=false;
+
+			rb.AddForce(new Vector3(60,40,0)*10);
+        }
+	}
+
+
+	void OnTriggerEnter(Collider colision)
+	{
+		if(colision.gameObject.tag=="Viento")
+		{
+			EnZonaViento=true;
+		}
+	}
+
+	void OnTriggerExit(Collider colision)
+	{
+		if(colision.gameObject.tag=="Viento")
+		{
+			EnZonaViento=false;
+		}
+	}
+}
